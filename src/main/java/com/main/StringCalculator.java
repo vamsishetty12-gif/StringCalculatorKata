f73558ec8219ce41adbeefa0000c6a4ef73efbf0
drenikunta <drenikunta@ctepl.com>
2025-10-28 18:20:15 +0530

    newline delimeter implementation

--- src/main/java/com/main/StringCalculator.java
+++ src/main/java/com/main/StringCalculator.java
@@ -4,8 +4,8 @@ public class StringCalculator {
 	
 	public int add(String numbers) {
 	    if (numbers.isEmpty()) return 0;
-
-	    String[] parts = numbers.split(",");
+	    
+	    String[] parts = numbers.split("[,\n]");
 	    int sum = 0;
 	    for (String num : parts) {
 	        sum += Integer.parseInt(num);
