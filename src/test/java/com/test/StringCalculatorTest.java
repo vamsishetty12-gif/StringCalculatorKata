46fb7f357e47dc0998e198b8f85df3e53532f970
drenikunta <drenikunta@ctepl.com>
2025-10-28 18:19:15 +0530

    newline delimeter test case

--- src/test/java/com/test/StringCalculatorTest.java
+++ src/test/java/com/test/StringCalculatorTest.java
@@ -32,6 +32,13 @@ public class StringCalculatorTest {
 	    StringCalculator calculator = new StringCalculator();
 	    assertEquals(10, calculator.add("1,2,3,4"));
 	}
+	
+	@Test
+	void testNewLineAsDelimiter() {
+	    StringCalculator calculator = new StringCalculator();
+	    assertEquals(6, calculator.add("1\n2,3"));
+	}
+
 
 
 }
